048c22c5027dc4f93cd54b5fa38711c336eacc25
marlaior <marlaior@hotmail.com>
2020-06-11 16:41:05 +0200

    crud songs into songlist

---
+++ api/src/main/java/com/ontimize/songslists/api/core/service/IList_SongListService.java
@@ -0,0 +1,22 @@
+package com.ontimize.songslists.api.core.service;
+
+import java.util.List;
+import java.util.Map;
+import com.ontimize.db.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
+
+public interface IList_SongListService {
+	
+	// LIST_SONGLISTS
+	
+	public EntityResult list_songlistQuery(Map<String, Object> keyMap, List<String> attrList)
+			throws OntimizeJEERuntimeException;
+
+	public EntityResult list_songlistInsert(Map<String, Object> attrMap) throws OntimizeJEERuntimeException;
+
+	public EntityResult list_songlistUpdate(Map<String, Object> attrMap, Map<String, Object> keyMap)
+			throws OntimizeJEERuntimeException;
+
+	public EntityResult list_songlistDelete(Map<String, Object> keyMap) throws OntimizeJEERuntimeException;
+
+}

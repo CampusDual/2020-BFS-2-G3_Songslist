edc7d744a6600763b4adbbda96cb29fd102f7a78
marlaior <marlaior@hotmail.com>
2020-06-15 01:31:57 +0200

    add id_user

--- model/src/main/java/com/ontimize/songslists/model/core/dao/UserDao.java
+++ model/src/main/java/com/ontimize/songslists/model/core/dao/UserDao.java
@@ -15,17 +15,18 @@ import com.ontimize.jee.server.dao.jdbc.OntimizeJdbcDaoSupport;
 	configurationFilePlaceholder = "dao/placeholders.properties")
 public class UserDao extends OntimizeJdbcDaoSupport {
 
-	//public static final String USR_EMAIL    = "email_user";
-	//public static final String USR_PASSWORD = "password_user";
+	public static final String USR_NICK    = "nick_user";
+	public static final String USR_PASSWORD = "password_user";
 
-	public static final String ID            = "user_";
+	public static final String ID            = "id_user";
+	public static final String NICK          = "nick_user";
 	public static final String EMAIL         = "email_user";
 	public static final String PASSWORD      = "password_user";
 	public static final String NAME          = "name_user";
 	public static final String SURNAME       = "surname_user";
 	public static final String BIRTHDAY      = "birthdate_user";
 	public static final String SCHEMA        = "db_schema";
-	public static final String CREATION_DATE = "user_creation_date";
-	public static final String DOWN_DATE     = "user_down_date";
+	public static final String CREATE_DATER  = "create_date_user";
+	public static final String DELETE_DATE   = "delete_date_user";
 
 }
--- ws/src/main/java/com/ontimize/songslists/ws/core/rest/SongRestController.java
+++ ws/src/main/java/com/ontimize/songslists/ws/core/rest/SongRestController.java
@@ -29,31 +29,29 @@ public class SongRestController extends ORestController<ISongService> {
 	@Autowired
 	private ISongService songService;
 
+	@Override
+	public ISongService getService() {
+		return this.songService;
+	}
 
- @Override
- public ISongService getService() {
-  return this.songService;
- }
- 
- @RequestMapping(value = "/searchSong", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/searchSong", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
 	public EntityResult currenSearch(@RequestBody Map<String, Object> req) {
 		try {
 			List<String> columns = (List<String>) req.get("columns");
 			Map<String, Object> filter = (Map<String, Object>) req.get("filter");
-			String nameToSearch = (String) filter.get("NAME");
+			String toSearch = (String) filter.get("NAME");
 			String option = (String) filter.get("OPTION");
 			if (!option.equals("")) {
-			Map<String, Object> key = new HashMap<String, Object>();
-			key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
-					searchLike(nameToSearch, option));
-			return songService.songQuery(key, columns);
-		}else {
-			Map<String, Object> key = new HashMap<String, Object>();
-			key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
-					searchAll(nameToSearch));
-			return songService.songQuery(key, columns);
-			
-		}
+				Map<String, Object> key = new HashMap<String, Object>();
+				key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY,
+						searchLike(toSearch, option));
+				return songService.songQuery(key, columns);
+			} else {
+				Map<String, Object> key = new HashMap<String, Object>();
+				key.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, searchAll(toSearch));
+				return songService.songQuery(key, columns);
+
+			}
 		} catch (Exception e) {
 			e.printStackTrace();
 			EntityResult res = new EntityResult();
@@ -67,45 +65,41 @@ public class SongRestController extends ORestController<ISongService> {
 		switch (option) {
 		case "song":
 			param = SongDao.ATTR_SONG_NAME;
-			// code block
 			break;
 		case "album":
 			param = SongDao.ATTR_ALBUM_NAME;
-			// code block
 			break;
 		case "artist":
 			param = SongDao.ATTR_ARTIST_NAME;
-			// code block
 			break;
-
-			case "genre":
+		case "genre":
 			param = SongDao.ATTR_GENRE_NAME;
-			// code block
 			break;
 		default:
-			// code block
+
 		}
 		BasicField field = new BasicField(param);
-		BasicExpression bexp1 = new BasicExpression(field, BasicOperator.LIKE_OP, "%"+toSearch+"%");
+		BasicExpression bexp1 = new BasicExpression(field, BasicOperator.LIKE_OP, "%" + toSearch + "%");
 		return bexp1;
 	}
-	private BasicExpression searchAll(String toSearch) {		
+
+	private BasicExpression searchAll(String toSearch) {
 		String param = SongDao.ATTR_SONG_NAME;
 		String param1 = SongDao.ATTR_ALBUM_NAME;
 		String param2 = SongDao.ATTR_ARTIST_NAME;
 		String param3 = SongDao.ATTR_GENRE_NAME;
 		BasicField field = new BasicField(param);
-		BasicExpression bexp = new BasicExpression(field, BasicOperator.LIKE_OP, "%"+toSearch+"%");
+		BasicExpression bexp = new BasicExpression(field, BasicOperator.LIKE_OP, "%" + toSearch + "%");
 		BasicField field1 = new BasicField(param1);
-		BasicExpression bexp1 = new BasicExpression(field1, BasicOperator.LIKE_OP, "%"+toSearch+"%");
+		BasicExpression bexp1 = new BasicExpression(field1, BasicOperator.LIKE_OP, "%" + toSearch + "%");
 		BasicField field2 = new BasicField(param2);
-		BasicExpression bexp2 = new BasicExpression(field2, BasicOperator.LIKE_OP, "%"+toSearch+"%");
+		BasicExpression bexp2 = new BasicExpression(field2, BasicOperator.LIKE_OP, "%" + toSearch + "%");
 		BasicField field3 = new BasicField(param3);
-		BasicExpression bexp3 = new BasicExpression(field3, BasicOperator.LIKE_OP, "%"+toSearch+"%");
-		BasicExpression bexp5 = new BasicExpression(bexp,BasicOperator.OR_OP,bexp1);
-		BasicExpression bexp6 = new BasicExpression(bexp5,BasicOperator.OR_OP,bexp2);
-		BasicExpression bexp7 = new BasicExpression(bexp6,BasicOperator.OR_OP,bexp3);
-	return  bexp7;
-}
+		BasicExpression bexp3 = new BasicExpression(field3, BasicOperator.LIKE_OP, "%" + toSearch + "%");
+		BasicExpression bexp5 = new BasicExpression(bexp, BasicOperator.OR_OP, bexp1);
+		BasicExpression bexp6 = new BasicExpression(bexp5, BasicOperator.OR_OP, bexp2);
+		BasicExpression bexp7 = new BasicExpression(bexp6, BasicOperator.OR_OP, bexp3);
+		return bexp7;
+	}
 
 }

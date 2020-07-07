9e47da45de6029a3e55856c80a0b825cca45335b
Bieito <miemail@example.com>
2020-07-07 23:13:44 +0200

    icos perfil add

--- frontend/src/main/resources/webapp/src/app/main/perfil/perfil.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/perfil/perfil.component.ts
@@ -14,6 +14,22 @@ import { SnackBarService, OSnackBarConfig } from 'ontimize-web-ngx';
   providers: [DatePipe]
 })
 export class PerfilComponent implements OnInit {
+  ico_png ='./assets/images/ico/png/';
+  public img1 = this.ico_png + '1.png';
+  public img2 = this.ico_png + '2.png';
+  public img3 = this.ico_png + '3.png';
+  public img4 = this.ico_png + '4.png';
+  public img5 = this.ico_png + '5.png';
+  public img6 = this.ico_png + '6.png';
+  public img7 = this.ico_png + '7.png';
+  public img8 = this.ico_png + '8.png';
+  public img9 = this.ico_png + '9.png';
+  public img10 = this.ico_png + '10.png';
+  public img11 = this.ico_png + '11.png';
+  public img12 = this.ico_png + '12.png';
+
+
+
   public perfilResult: IUserModel = null;
   // public perfiUpdate: IUserModel ;
   public contactForm: FormGroup = null;

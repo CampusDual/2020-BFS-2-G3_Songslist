7d586dd3ff5b4966705102801297c5b9a5696be4
marlaior <marlaior@hotmail.com>
2020-07-09 17:04:40 +0200

    fix

--- frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
+++ frontend/src/main/resources/webapp/src/app/shared/app.menu.config.ts
@@ -2,6 +2,7 @@ import { MenuRootItem } from 'ontimize-web-ngx';
 
 export const MENU_CONFIG: MenuRootItem[] = [
   { id: 'home', name: 'HOME', icon: 'home', route: '/main/home' },
-  { id: 'perfil-edit', name: 'Edit Perfil', icon:'login', route: '/main/perfil', confirm: 'yes' },
+  { id: 'songlist', name: 'MY_SONGLISTS', icon: 'list', route: '/main/songlist'},
+  { id: 'perfil-edit', name: 'EDIT_PROFILE', icon:'user', route: '/main/perfil'},
   { id: 'logout', name: 'LOGOUT', route: '/login', icon: 'power_settings_new', confirm: 'yes' }
 ];

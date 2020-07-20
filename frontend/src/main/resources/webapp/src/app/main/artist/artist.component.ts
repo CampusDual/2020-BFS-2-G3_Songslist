a7110a5dd3d61e7f4f30d2d90c3acb18567b3187
marlaior <marlaior@hotmail.com>
2020-07-20 02:21:02 +0200

    grid artist

--- frontend/src/main/resources/webapp/src/app/main/artist/artist.component.ts
+++ frontend/src/main/resources/webapp/src/app/main/artist/artist.component.ts
@@ -5,87 +5,88 @@ import { ArtistService } from '../services/artist.service';
 import { IAlbumModel } from 'app/shared/models/ialbum.model';
 
 @Component({
-  selector: 'app-artist',
-  templateUrl: './artist.component.html',
-  styleUrls: ['./artist.component.scss']
+    selector: 'app-artist',
+    templateUrl: './artist.component.html',
+    styleUrls: ['./artist.component.scss']
 })
 export class ArtistComponent implements OnInit {
-  public parametro: any;
-  public artistResult: IArtistModel;
-  public albumResult: IAlbumModel;
+    public parametro: any;
+    public artistResult: IArtistModel;
+    public albumResult: IAlbumModel;
+    public dateAlbum: Date;
 
-  constructor(
+    constructor(
 
-    private _route: ActivatedRoute,
+        private _route: ActivatedRoute,
         private artistService: ArtistService
-  ) { }
+    ) { }
 
-  ngOnInit() {
+    ngOnInit() {
 
-    this._route.params.forEach((params: Params) => {
-      this.parametro = params['id'];
+        this._route.params.forEach((params: Params) => {
+            this.parametro = params['id'];
 
-  });
-  this.ngOnStartArtist(this.parametro);
-  this.ngOnStartArtistAlbumlist(this.parametro);
+        });
+        this.ngOnStartArtist(this.parametro);
+        this.ngOnStartArtistAlbumlist(this.parametro);
 
-  }
-  ngOnStartArtist(id: number) {
+    }
+    ngOnStartArtist(id: number) {
 
-    this.artistService.getArtistData(id).subscribe(
+        this.artistService.getArtistData(id).subscribe(
 
-        (artistData: any) => {
+            (artistData: any) => {
 
-            if (artistData['data']) {
+                if (artistData['data']) {
 
-                if (artistData['data'].length > 0) {
+                    if (artistData['data'].length > 0) {
 
-                    this.artistResult = artistData['data'][0];
+                        this.artistResult = artistData['data'][0];
 
-                } else {
-                    this.artistResult = null;
+                    } else {
+                        this.artistResult = null;
+                    }
                 }
-            }
-        },
-        err => console.error(err)
-    );
-}
-ngOnStartArtistAlbumlist(id: number) {
+            },
+            err => console.error(err)
+        );
+    }
+    ngOnStartArtistAlbumlist(id: number) {
 
-  this.artistService.getArtistAlbumlist(id).subscribe(
+        this.artistService.getArtistAlbumlist(id).subscribe(
 
-      (albumlist: any) => {
+            (albumlist: any) => {
 
-          if (albumlist['data']) {
+                if (albumlist['data']) {
 
-              if (albumlist['data'].length > 0) {
+                    if (albumlist['data'].length > 0) {
 
-                  this.albumResult = albumlist['data'];
-                  console.log('DATA ALBUMLIST ', albumlist['data']);
-                  console.log('ALBUMLIST ', this.albumResult);
+                        this.albumResult = albumlist['data'];
+                        console.log('DATA ALBUMLIST ', albumlist['data']);
+                        console.log('ALBUMLIST ', this.albumResult);
 
-              } else {
-                  this.albumResult = null;
-              }
-          }
+                    } else {
+                        this.albumResult = null;
+                    }
+                }
 
-      },
-      err => console.error(err)
+            },
+            err => console.error(err)
 
-  );
-  console.log('fuera del subscribe', this.albumResult);
+        );
+        console.log('fuera del subscribe', this.albumResult);
 
-}
-getAlbumResult(){
-    console.log('Contenido de album', this.albumResult);
-    return this.albumResult;
+    }
+    getAlbumResult() {
+        console.log('Contenido de album', this.albumResult);
+        return this.albumResult;
 
-}
+    }
 
-getArtistDescription(){
-    console.log('Contenido de descripción', this.artistResult);
-    return this.artistResult.description_artist;
-}
+    getArtistDescription() {
+        console.log('Contenido de descripción', this.artistResult);
+        return this.artistResult.description_artist;
+    }
 
 
 }
